bd2b1d91976849be529b37c1e733d5710f485ff7
RadaRish <107124341+RadaRish@users.noreply.github.com>
2024-01-20 13:16:38 +0300

    Subnova Tour

---
+++ data.js
@@ -0,0 +1,205 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--1",
+      "name": "Точка 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.8417949053042761,
+        "pitch": -0.0862615257453605,
+        "fov": 1.5537519122748535
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3951930079858048,
+          "pitch": 0.10315240782605706,
+          "rotation": 0,
+          "target": "1--2"
+        },
+        {
+          "yaw": 0.1764745235143792,
+          "pitch": 0.07562339395282791,
+          "rotation": 0,
+          "target": "3--4"
+        },
+        {
+          "yaw": -1.136602940620957,
+          "pitch": 0.06180829870315918,
+          "rotation": 0,
+          "target": "2--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--2",
+      "name": "Точка 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.4777535789127079,
+        "pitch": -0.15971319489521818,
+        "fov": 1.5537519122748535
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0291216453436896,
+          "pitch": 0.3118639512487533,
+          "rotation": 0,
+          "target": "0--1"
+        },
+        {
+          "yaw": 0.6585633535238031,
+          "pitch": 0.11619770179624744,
+          "rotation": 0,
+          "target": "3--4"
+        },
+        {
+          "yaw": -1.5382373262959508,
+          "pitch": 0.05021066360468396,
+          "rotation": 0,
+          "target": "2--3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--3",
+      "name": "Точка 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.586320755830144,
+        "pitch": 0.14701078452192995,
+        "fov": 1.5537519122748535
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0720844112378316,
+          "pitch": 0.217244665783479,
+          "rotation": 0,
+          "target": "0--1"
+        },
+        {
+          "yaw": -0.2324117121402729,
+          "pitch": 0.1675727439417063,
+          "rotation": 0,
+          "target": "1--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--4",
+      "name": "Точка 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.0069909661778382,
+        "pitch": 0.032950693082495874,
+        "fov": 1.5537519122748535
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3437537619990465,
+          "pitch": 0.10876891119927379,
+          "rotation": 0,
+          "target": "1--2"
+        },
+        {
+          "yaw": -1.208825138202041,
+          "pitch": 0.11205365807401613,
+          "rotation": 0,
+          "target": "0--1"
+        },
+        {
+          "yaw": -0.8442870886261282,
+          "pitch": 0.05198565780077935,
+          "rotation": 0,
+          "target": "2--3"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
